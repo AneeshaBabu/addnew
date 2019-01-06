4f7bb149b37921e3e25a11a9c67dcffba269b1a8
AneeshaBabu <aneeshababu051@gmail.com>
2019-01-06 13:10:49 +0530

    'threenumber'

--- app/src/main/java/com/ihrd/aneeshaalbin/addnew/MainActivity.java
+++ app/src/main/java/com/ihrd/aneeshaalbin/addnew/MainActivity.java
@@ -9,18 +9,44 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Button b;
+    EditText ed1,ed2,ed3,ed4;
+    String s1,s2,s3;
+    int a1,a2,a3;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=(Button)findViewById(R.id.next);
+        ed1=(EditText)findViewById(R.id.numone);
+        ed2=(EditText)findViewById(R.id.numtwo);
+        ed3=(EditText)findViewById(R.id.numthree);
+        ed4=(EditText)findViewById(R.id.result);
+        b=(Button)findViewById(R.id.biggest);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
-                Intent intent=new Intent(getApplicationContext(),WelcomeActivity.class);
-                startActivity(intent);
+            public void onClick(View v)
+            {
+                s1=ed1.getText().toString();
+              s2=ed2.getText().toString();
+              s3=ed3.getText().toString();
+              a1=Integer.parseInt(s1);
+              a2=Integer.parseInt(s2);
+              a3=Integer.parseInt(s3);
+                if((a1>a2)&&(a1>a3))
+                {
+                    ed4.setVisibility(View.VISIBLE);
+                    ed4.setText(s1);
+                }
+                else if((a2>a1)&&(a2>a3))
+                {
+                    ed4.setVisibility(View.VISIBLE);
+                    ed4.setText(s2);
+                }
+                else
+                {
+                    ed4.setVisibility(View.VISIBLE);
+                    ed4.setText(s3);
+                }
             }
         });
     }
